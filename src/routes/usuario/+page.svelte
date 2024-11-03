8d2c58d8f7b0bf0c286a10714a0ca8716afa0d95
Juan Charris <Juan.Charris2110@gmail.com>
2024-11-02 22:22:23 -0500

    Crear Usuario

---
+++ src/routes/usuario/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    import Corrousel from "../lib/components/corrousel.svelte";
+    import Footer from "../lib/components/footer.svelte";
+    import Icons from "../lib/components/icons.svelte";
+    import Navbar from "../lib/components/Navbar.svelte";
+    import Productos from "../lib/components/Productos.svelte";
+    import Servicios from "../lib/components/Servicios.svelte";
+    import "bootstrap-icons/font/bootstrap-icons.css";
+</script>
+
+<Navbar></Navbar>
+
+<Corrousel></Corrousel>
+<p>USUARIO NUEVO</p>
+<Servicios></Servicios>
+<Icons></Icons>
+
+<Productos></Productos>
+<Footer></Footer>
