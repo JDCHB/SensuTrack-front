5c9cde5b04b91809e744d60b36301afb407de225
Juan Charris <Juan.Charris2110@gmail.com>
2025-02-12 19:07:36 -0500

    a

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -2,5 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	build: {
+		outDir: 'build', // Aquí especificamos el directorio de salida
+	}
 });
