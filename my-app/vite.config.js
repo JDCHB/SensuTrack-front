c46dda8c14b0a232de5565685d19c39fd5b44784
Juan Charris <Juan.Charris2110@gmail.com>
2025-02-12 19:59:38 -0500

    WQWQW

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -3,7 +3,11 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	server: {
+		port: process.env.PORT || 4173, // Usa el puerto proporcionado por Render o 4173 como fallback
+		host: '0.0.0.0', // Hace que la aplicación esté accesible desde cualquier IP
+	},
 	build: {
-		outDir: 'build', // Aquí especificamos el directorio de salida
-	}
+		outDir: 'build', // Especificamos el directorio de salida para la construcción
+	},
 });
