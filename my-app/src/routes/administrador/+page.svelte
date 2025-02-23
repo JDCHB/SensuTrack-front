fd2f93d83f923d9f532432672830fc887881e0be
Juan Charris <Juan.Charris2110@gmail.com>
2025-02-22 22:54:19 -0500

    modificacion del menu del admin, se le coloco una imagen de fondo

--- my-app/src/routes/administrador/+page.svelte
+++ my-app/src/routes/administrador/+page.svelte
@@ -40,101 +40,104 @@
 </script>
 
 <!-- HTML del menú de administrador -->
-<div class="container mt-5 pt-1">
-    <div class="row justify-content-center">
-        <div class="col-md-8 col-lg-6">
-            <div class="admin-menu bg-white shadow-lg rounded p-4">
-                <h2 class="text-center mb-4 text-primary">
-                    Menú de Super_Administrador
-                </h2>
+<div class="fondo">
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-8 col-lg-6">
+                <div class="admin-menu">
+                    <h2 class="text-center mb-4 text-primary">
+                        Menú de Super_Administrador
+                    </h2>
 
-                <div class="text-center mb-4">
-                    <p class="lead">
-                        Bienvenido, Super_Admin. ¿A qué página deseas ingresar?
-                    </p>
-                </div>
+                    <div class="text-center mb-4">
+                        <p class="lead">
+                            Bienvenido, Super_Admin. ¿A qué página deseas
+                            ingresar?
+                        </p>
+                    </div>
 
-                <div class="row">
-                    <div class="col-6 mb-3">
-                        <button
-                            on:click={mostrarConfirmacionUser}
-                            class="btn btn-lg btn-outline-primary d-flex align-items-center justify-content-between w-100"
-                        >
-                            <span>VISTA DE USUARIO</span>
-                            <i class="bi bi-person-circle"></i>
-                        </button>
-                    </div>
-                    <div class="col-6 mb-3">
-                        <button
-                            class="btn btn-lg btn-outline-success d-flex align-items-center justify-content-between w-100"
-                            on:click={mostrarConfirmacionRegistroUsuario}
-                        >
-                            <span>REGISTRAR USUARIO</span>
-                            <i class="bi bi-person-plus"></i>
-                        </button>
-                    </div>
-                    <div class="col-6 mb-3">
-                        <button
-                            class="btn btn-lg btn-outline-dark d-flex align-items-center justify-content-between w-100"
-                            on:click={mostrarConfirmacionReporte}
-                        >
-                            <span>REPORTES</span>
-                            <i class="bi bi-gear-fill"></i>
-                        </button>
-                    </div>
-                    <div class="col-6 mb-3">
-                        <button
-                            class="btn btn-lg btn-outline-warning d-flex align-items-center justify-content-between w-100"
-                            on:click={mostrarConfirmacionRegistroGPS}
-                        >
-                            <span>REGISTRAR GPS</span>
-                            <i class="bi bi-plus-circle"></i>
-                        </button>
-                    </div>
-                    <div class="col-6 mb-3">
-                        <button
-                            class="btn btn-lg btn-outline-primary d-flex align-items-center justify-content-between w-100"
-                            on:click={mostrarTablaUsuarios}
-                        >
-                            <span>TABLA USUARIOS</span>
-                            <i class="bi bi-person"></i>
-                        </button>
-                    </div>
-                    <div class="col-6 mb-3">
-                        <button
-                            class="btn btn-lg btn-outline-success d-flex align-items-center justify-content-between w-100"
-                            on:click={mostrarRegistroRoles}
-                        >
-                            <span>REGISTRAR ROLES</span>
-                            <i class="bi bi-person-plus"></i>
-                        </button>
-                    </div>
-                    <div class="col-6 mb-3">
-                        <button
-                            class="btn btn-lg btn-outline-dark d-flex align-items-center justify-content-between w-100"
-                            on:click={mostrarRegistroModulos}
-                        >
-                            <span>REGISTRAR MODULOS</span>
-                            <i class="bi bi-gear-fill"></i>
-                        </button>
-                    </div>
-                    <div class="col-6 mb-3">
-                        <button
-                            class="btn btn-lg btn-outline-warning d-flex align-items-center justify-content-between w-100"
-                            on:click={mostrarRegistroModuloxRol}
-                        >
-                            <span>MODULOXROL</span>
-                            <i class="bi bi-plus-circle"></i>
-                        </button>
-                    </div>
-                    <div class="col-6 mb-3">
-                        <button
-                            class="btn btn-lg btn-outline-danger d-flex align-items-center justify-content-between w-100"
-                            on:click={logout}
-                        >
-                            <span>CERRAR SESION</span>
-                            <i class="bi bi-power"></i>
-                        </button>
+                    <div class="row">
+                        <div class="col-6 mb-3">
+                            <button
+                                on:click={mostrarConfirmacionUser}
+                                class="btn btn-lg btn-outline-primary d-flex align-items-center justify-content-between w-100"
+                            >
+                                <span>VISTA DE USUARIO</span>
+                                <i class="bi bi-person-circle"></i>
+                            </button>
+                        </div>
+                        <div class="col-6 mb-3">
+                            <button
+                                class="btn btn-lg btn-outline-success d-flex align-items-center justify-content-between w-100"
+                                on:click={mostrarConfirmacionRegistroUsuario}
+                            >
+                                <span>REGISTRAR USUARIO</span>
+                                <i class="bi bi-person-plus"></i>
+                            </button>
+                        </div>
+                        <div class="col-6 mb-3">
+                            <button
+                                class="btn btn-lg btn-outline-dark d-flex align-items-center justify-content-between w-100"
+                                on:click={mostrarConfirmacionReporte}
+                            >
+                                <span>REPORTES</span>
+                                <i class="bi bi-gear-fill"></i>
+                            </button>
+                        </div>
+                        <div class="col-6 mb-3">
+                            <button
+                                class="btn btn-lg btn-outline-warning d-flex align-items-center justify-content-between w-100"
+                                on:click={mostrarConfirmacionRegistroGPS}
+                            >
+                                <span>REGISTRAR GPS</span>
+                                <i class="bi bi-plus-circle"></i>
+                            </button>
+                        </div>
+                        <div class="col-6 mb-3">
+                            <button
+                                class="btn btn-lg btn-outline-primary d-flex align-items-center justify-content-between w-100"
+                                on:click={mostrarTablaUsuarios}
+                            >
+                                <span>TABLA USUARIOS</span>
+                                <i class="bi bi-person"></i>
+                            </button>
+                        </div>
+                        <div class="col-6 mb-3">
+                            <button
+                                class="btn btn-lg btn-outline-success d-flex align-items-center justify-content-between w-100"
+                                on:click={mostrarRegistroRoles}
+                            >
+                                <span>REGISTRAR ROLES</span>
+                                <i class="bi bi-person-plus"></i>
+                            </button>
+                        </div>
+                        <div class="col-6 mb-3">
+                            <button
+                                class="btn btn-lg btn-outline-dark d-flex align-items-center justify-content-between w-100"
+                                on:click={mostrarRegistroModulos}
+                            >
+                                <span>REGISTRAR MODULOS</span>
+                                <i class="bi bi-gear-fill"></i>
+                            </button>
+                        </div>
+                        <div class="col-6 mb-3">
+                            <button
+                                class="btn btn-lg btn-outline-warning d-flex align-items-center justify-content-between w-100"
+                                on:click={mostrarRegistroModuloxRol}
+                            >
+                                <span>MODULOXROL</span>
+                                <i class="bi bi-plus-circle"></i>
+                            </button>
+                        </div>
+                        <div class="col-6 mb-3">
+                            <button
+                                class="btn btn-lg btn-outline-danger d-flex align-items-center justify-content-between w-100"
+                                on:click={logout}
+                            >
+                                <span>CERRAR SESION</span>
+                                <i class="bi bi-power"></i>
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -149,12 +152,21 @@
 />
 
 <style>
+    .fondo {
+        display: flex; /* Usamos flexbox */
+        justify-content: center; /* Centra el contenido horizontalmente */
+        align-items: center; /* Centra el contenido verticalmente */
+        background-image: url("/FONDO-ADMIN.jpg");
+        background-size: cover; /* Ajusta la imagen al tamaño del contenedor */
+        background-position: center;
+        background-repeat: no-repeat;
+        height: 100vh; /* Asegura que la imagen ocupe toda la pantalla */
+    }
+
     /* Diseño para el menú de administrador */
     .admin-menu {
-        background-color: #f9f9f9;
         border-radius: 15px;
         padding: 30px;
-        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
     }
 
     .admin-menu h2 {
