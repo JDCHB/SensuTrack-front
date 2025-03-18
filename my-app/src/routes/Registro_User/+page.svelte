12030a2bf474fcccf65c785bddd7332e88f63bee
Juan Charris <Juan.Charris2110@gmail.com>
2025-03-18 15:35:11 -0500

    xd2

--- my-app/src/routes/Registro_User/+page.svelte
+++ my-app/src/routes/Registro_User/+page.svelte
@@ -545,13 +545,10 @@
                 <div class="modal-footer">
                     <button
                         type="button"
-                        class="btn btn-outline-secondary"
+                        class="btn btn-primary"
                         data-bs-dismiss="modal"
                     >
                         Cerrar
-                    </button>
-                    <button type="button" class="btn btn-primary">
-                        Aceptar
                     </button>
                 </div>
             </div>
