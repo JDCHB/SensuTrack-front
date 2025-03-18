70163ca7ab2db67c3b889e78dbfae6c3a271b367
Juan Charris <Juan.Charris2110@gmail.com>
2025-03-18 14:12:55 -0500

    s

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -15,20 +15,9 @@ export default defineConfig({
 	},
 });
 */
-// import { sveltekit } from '@sveltejs/kit/vite';
-// import { defineConfig } from 'vite';
-
-// export default defineConfig({
-// 	plugins: [sveltekit()]
-// });
-
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		host: '0.0.0.0', // Escucha en todas las interfaces de red
-		port: process.env.PORT || 4173, // Usa el puerto asignado por Render o el puerto 4173 por defecto
-	},
+	plugins: [sveltekit()]
 });
