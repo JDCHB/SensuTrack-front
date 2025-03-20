f26c0695c5325bdd8d2b6b15c9c655d00e96a850
Juan Charris <Juan.Charris2110@gmail.com>
2025-03-20 18:02:37 -0500

    a

--- my-app/src/routes/usuario/+page.svelte
+++ my-app/src/routes/usuario/+page.svelte
@@ -7,6 +7,41 @@
     import Servicios from "../../lib/components/Servicios.svelte";
     import Footer from "../../lib/components/footer.svelte";
     import "bootstrap-icons/font/bootstrap-icons.css";
+
+    onMount(async () => {
+        try {
+            // Verifica si hay una sesión de Google en las cookies
+            let cookies = document.cookie
+                .split("; ")
+                .find((row) => row.startsWith("sesionGoogle="));
+
+            if (cookies) {
+                let sesionGoogleRaw = cookies.split("=")[1];
+                let sesionGoogle = JSON.parse(
+                    decodeURIComponent(sesionGoogleRaw),
+                );
+
+                // Guarda los datos de la sesión en el localStorage
+                console.log("Sesión decodificada:", sesionGoogle);
+                let miStorage = window.localStorage;
+                let name = sesionGoogle.nombre;
+                let apellido = sesionGoogle.apellido;
+                let id = sesionGoogle.id;
+                let correo = sesionGoogle.email;
+                let user_data = { name, id, correo };
+
+                miStorage.setItem("user_data", JSON.stringify(user_data));
+                // Actualiza los campos del formulario con los datos de Google
+                document.getElementById("v_nombre").value = name;
+                document.getElementById("v_apellido").value = apellido;
+                document.getElementById("v_email").value = correo;
+            }
+        } catch (e) {
+            error = e.message;
+        } finally {
+            loading = false;
+        }
+    });
 </script>
 
 <NavbarUser></NavbarUser>
