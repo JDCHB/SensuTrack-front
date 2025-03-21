e0a68163345487ecbd03f2071191aa79584b9d05
Juan Charris <Juan.Charris2110@gmail.com>
2025-03-20 19:44:41 -0500

    a

--- my-app/src/routes/usuario/+page.svelte
+++ my-app/src/routes/usuario/+page.svelte
@@ -11,105 +11,107 @@
     let loading;
     let correo = "";
     onMount(async () => {
-        try {
-            // Verifica si hay una sesión de Google en las cookies
-            let cookies = document.cookie
-                .split("; ")
-                .find((row) => row.startsWith("sesionGoogle="));
+        setTimeout(async () => {
+            try {
+                // Verifica si hay una sesión de Google en las cookies
+                let cookies = document.cookie
+                    .split("; ")
+                    .find((row) => row.startsWith("sesionGoogle="));
 
-            if (cookies) {
-                let sesionGoogleRaw = cookies.split("=")[1];
-                let sesionGoogle = JSON.parse(
-                    decodeURIComponent(sesionGoogleRaw),
-                );
+                if (cookies) {
+                    let sesionGoogleRaw = cookies.split("=")[1];
+                    let sesionGoogle = JSON.parse(
+                        decodeURIComponent(sesionGoogleRaw),
+                    );
 
-                // Guarda los datos de la sesión en el localStorage
-                console.log("Sesión decodificada:", sesionGoogle);
-                let miStorage = window.localStorage;
-                let name = sesionGoogle.nombre;
-                let apellido = sesionGoogle.apellido;
-                let id = sesionGoogle.id;
-                correo = sesionGoogle.email;
-                console.log(correo);
-                let user_data = { name, id, correo };
+                    // Guarda los datos de la sesión en el localStorage
+                    console.log("Sesión decodificada:", sesionGoogle);
+                    let miStorage = window.localStorage;
+                    let name = sesionGoogle.nombre;
+                    let apellido = sesionGoogle.apellido;
+                    let id = sesionGoogle.id;
+                    correo = sesionGoogle.email;
+                    console.log(correo);
+                    let user_data = { name, id, correo };
 
-                const response = await fetch(
-                    "https://proyectomascotas.onrender.com/generate_token_google",
-                    {
-                        method: "POST",
-                        headers: {
-                            "Content-Type": "application/json",
+                    const response = await fetch(
+                        "https://proyectomascotas.onrender.com/generate_token_google",
+                        {
+                            method: "POST",
+                            headers: {
+                                "Content-Type": "application/json",
+                            },
+                            body: JSON.stringify({
+                                email: correo,
+                            }),
                         },
-                        body: JSON.stringify({
-                            email: correo,
-                        }),
-                    },
-                );
-                const data = await response.json();
-                console.log(data);
-                if (response.ok) {
-                    const { access_token } = data;
-                    localStorage.setItem("access_token", access_token);
-                } else {
-                    Swal.fire({
-                        icon: "error",
-                        title: "Oops...",
-                        text:
-                            data.detail ||
-                            "Ha ocurrido un error al generar el token.",
-                        customClass: {
-                            popup: "swal-popup", // Clase para personalizar el popup de la alerta
-                            title: "custom-title", // Clase personalizada para el título
-                        },
-                    });
+                    );
+                    const data = await response.json();
+                    console.log(data);
+                    if (response.ok) {
+                        const { access_token } = data;
+                        localStorage.setItem("access_token", access_token);
+                    } else {
+                        Swal.fire({
+                            icon: "error",
+                            title: "Oops...",
+                            text:
+                                data.detail ||
+                                "Ha ocurrido un error al generar el token.",
+                            customClass: {
+                                popup: "swal-popup", // Clase para personalizar el popup de la alerta
+                                title: "custom-title", // Clase personalizada para el título
+                            },
+                        });
+                    }
+
+                    miStorage.setItem("user_data", JSON.stringify(user_data));
                 }
-
-                miStorage.setItem("user_data", JSON.stringify(user_data));
+            } catch (e) {
+                error = e.message;
+            } finally {
+                loading = false;
             }
-        } catch (e) {
-            error = e.message;
-        } finally {
-            loading = false;
-        }
 
-        // try {
-        //     const response = await fetch(
-        //         "https://proyectomascotas.onrender.com/generate_token_google",
-        //         {
-        //             method: "POST",
-        //             headers: {
-        //                 "Content-Type": "application/json",
-        //             },
-        //             body: JSON.stringify({}),
-        //         },
-        //     );
-        //     const data = await response.json();
+            // try {
+            //     const response = await fetch(
+            //         "https://proyectomascotas.onrender.com/generate_token_google",
+            //         {
+            //             method: "POST",
+            //             headers: {
+            //                 "Content-Type": "application/json",
+            //             },
+            //             body: JSON.stringify({}),
+            //         },
+            //     );
+            //     const data = await response.json();
 
-        //     if (response.ok) {
-        //         const { access_token } = data;
-        //         localStorage.setItem("access_token", access_token);
-        //     } else {
-        //         Swal.fire({
-        //             icon: "error",
-        //             title: "Oops...",
-        //             text:
-        //                 data.detail ||
-        //                 "Ha ocurrido un error al generar el token.",
-        //             customClass: {
-        //                 popup: "swal-popup", // Clase para personalizar el popup de la alerta
-        //                 title: "custom-title", // Clase personalizada para el título
-        //             },
-        //         });
-        //     }
-        // } catch (error) {
-        //     console.error("Error en la solicitud:", e.message);
-        //     hideLoader(loginLoader);
-        //     Swal.fire({
-        //         icon: "error",
-        //         title: "Error",
-        //         text: "Hubo un problema al crear el Token.",
-        //     });
-        // }
+            //     if (response.ok) {
+            //         const { access_token } = data;
+            //         localStorage.setItem("access_token", access_token);
+            //     } else {
+            //         Swal.fire({
+            //             icon: "error",
+            //             title: "Oops...",
+            //             text:
+            //                 data.detail ||
+            //                 "Ha ocurrido un error al generar el token.",
+            //             customClass: {
+            //                 popup: "swal-popup", // Clase para personalizar el popup de la alerta
+            //                 title: "custom-title", // Clase personalizada para el título
+            //             },
+            //         });
+            //     }
+            // } catch (error) {
+            //     console.error("Error en la solicitud:", e.message);
+            //     hideLoader(loginLoader);
+            //     Swal.fire({
+            //         icon: "error",
+            //         title: "Error",
+            //         text: "Hubo un problema al crear el Token.",
+            //     });
+            // }
+        }, 500);
     });
 </script>
 
